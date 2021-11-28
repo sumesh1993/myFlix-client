6014c457d759feb7d485f8b7427db46999b6745f
rusevaIvelina <iva.ruseva14@gmail.com>
2021-11-28 18:04:25 -0500

    updated the movie descriptions and added genre property

--- dist/index.6701a6e1.js
+++ dist/index.6701a6e1.js
@@ -22751,19 +22751,22 @@ class MainView extends _reactDefault.default.Component {
                 {
                     _id: 1,
                     Title: 'Goodfellas',
-                    Description: ' movie description1',
+                    Genre: 'drama',
+                    Description: ' The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate.',
                     ImagePath: 'https://www.imdb.com/title/tt0099685/mediaviewer/rm2091797760/'
                 },
                 {
                     _id: 2,
                     Title: 'Titanic',
-                    Description: 'movie description2',
+                    Genre: 'drama',
+                    Description: 'A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.',
                     ImagePath: 'https://www.imdb.com/title/tt0120338/mediaviewer/rm2647458304/'
                 },
                 {
                     _id: 3,
                     Title: 'Pulp Fiction',
-                    Description: 'movie description3',
+                    Genre: 'crime',
+                    Description: 'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption',
                     ImagePath: 'https://www.imdb.com/title/tt0110912/mediaviewer/rm1959546112/'
                 }
             ],
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,9 +8,9 @@ import { MovieView } from '../movie-view/movie-view';
         super();
         this.state = {
             movies: [
-                {_id: 1, Title: 'Goodfellas', Description: ' movie description1', ImagePath: 'https://www.imdb.com/title/tt0099685/mediaviewer/rm2091797760/'},
-                {_id: 2, Title: 'Titanic', Description: 'movie description2', ImagePath: 'https://www.imdb.com/title/tt0120338/mediaviewer/rm2647458304/'},
-                {_id: 3, Title: 'Pulp Fiction', Description: 'movie description3', ImagePath: 'https://www.imdb.com/title/tt0110912/mediaviewer/rm1959546112/'}
+                {_id: 1, Title: 'Goodfellas', Genre: 'drama', Description: ' The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate.', ImagePath: 'https://www.imdb.com/title/tt0099685/mediaviewer/rm2091797760/'},
+                {_id: 2, Title: 'Titanic', Genre: 'drama', Description: 'A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.', ImagePath: 'https://www.imdb.com/title/tt0120338/mediaviewer/rm2647458304/'},
+                {_id: 3, Title: 'Pulp Fiction', Genre: 'crime', Description: 'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption', ImagePath: 'https://www.imdb.com/title/tt0110912/mediaviewer/rm1959546112/'}
             ],
             selectedMovie: null
         };
