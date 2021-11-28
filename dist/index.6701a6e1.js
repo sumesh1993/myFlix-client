5e0670dba4478e547f3de064f70d2e3909502860
rusevaIvelina <iva.ruseva14@gmail.com>
2021-11-28 18:41:00 -0500

    replaced the ImagePath links

--- dist/index.6701a6e1.js
+++ dist/index.6701a6e1.js
@@ -22752,19 +22752,19 @@ class MainView extends _reactDefault.default.Component {
                     _id: 1,
                     Title: 'Goodfellas',
                     Description: ' The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate.',
-                    ImagePath: 'https://www.imdb.com/title/tt0099685/mediaviewer/rm2091797760/'
+                    ImagePath: '	https://m.media-amazon.com/images/M/MV5BY2NkZjEzMD…YjFjNmI3ZGEwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg'
                 },
                 {
                     _id: 2,
                     Title: 'Titanic',
                     Description: 'A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.',
-                    ImagePath: 'https://www.imdb.com/title/tt0120338/mediaviewer/rm2647458304/'
+                    ImagePath: 'https://m.media-amazon.com/images/M/MV5BMDdmZGU3ND…ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg'
                 },
                 {
                     _id: 3,
                     Title: 'Pulp Fiction',
                     Description: 'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption',
-                    ImagePath: 'https://www.imdb.com/title/tt0110912/mediaviewer/rm1959546112/'
+                    ImagePath: '	https://m.media-amazon.com/images/M/MV5BNGNhMDIzZT…MjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg'
                 }
             ],
             selectedMovie: null
