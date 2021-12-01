1a1adda67e5243f48cd9897e92b7dcc0aa0c736b
rusevaIvelina <iva.ruseva14@gmail.com>
2021-12-01 11:20:39 -0500

    added componentDidMount

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -7,13 +9,21 @@ import { MovieView } from '../movie-view/movie-view';
     constructor() {
         super();
         this.state = {
-            movies: [
-                {_id: 1, Title: 'Goodfellas', Description: ' The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BY2NkZjEzMD…YjFjNmI3ZGEwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg'},
-                {_id: 2, Title: 'Titanic', Description: 'A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BMDdmZGU3ND…ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg'},
-                {_id: 3, Title: 'Pulp Fiction', Description: 'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption', ImagePath: 'https://m.media-amazon.com/images/M/MV5BNGNhMDIzZT…MjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg'}
-            ],
+            movies: [],
             selectedMovie: null
         };
+    }
+
+    componentDidMount() {
+      axios.get ('https://myflix-movietime.herokuapp.com/movies')
+      .then(response => {
+        this.setState( {
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
     }
 
     setSelectedMovie(newSelectedMovie) {
@@ -32,9 +42,11 @@ import { MovieView } from '../movie-view/movie-view';
          return (
             <div className="main-view">
               {selectedMovie
-                ? <MovieView movie = {selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+                ? <MovieView movie = {selectedMovie} onBackClick={newSelectedMovie => 
+                  { this.setSelectedMovie(newSelectedMovie); }} />
                 : movies.map(movie => (
-                  <MovieCard key = {movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
+                  <MovieCard key = {movie._id} movie={movie} onMovieClick={(newSelectedMovie) => 
+                    { this.setSelectedMovie(newSelectedMovie) }} />
                   ))
               }
             </div>
