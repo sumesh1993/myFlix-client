0e8dd368f95fc61a0af1da49734564f4077bbd00
rusevaIvelina <iva.ruseva14@gmail.com>
2021-12-05 15:43:28 -0500

    replaced crossorigin with crossOrigin

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -8,7 +8,7 @@ export class MovieView extends React.Component {
         return (
             <div className="movie-view">
                 <div className="movie-poster">
-                    <img src={movie.ImagePath} crossorigin="anonymous" />
+                    <img src={movie.ImagePath} crossOrigin="anonymous" />
                 </div>
                 <div className="movie-title">
                     <span className="label">Title: </span>
