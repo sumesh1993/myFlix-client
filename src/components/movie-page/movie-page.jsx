abf738a8c8d33f60dad2655c8d7e0dbc5fbe04fc
rusevaIvelina <iva.ruseva14@gmail.com>
2021-12-18 10:02:18 -0500

    created movie-page

---
+++ src/components/movie-page/movie-page.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { LoginView } from '../login-view/login-view';
+import { Navbar } from '../navbar/navbar';
+import { MovieView } from '../movie-view/movie-view';
+
+export class MoviePage extends React.Component {
+    render() {
+       if (!user) return <Col>
+         <Navbar/>
+           <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
+              </Col>
+        if (movies.length === 0) return <div className="main-view" />;
+            return <Col md={8}>
+            <MovieView movie={movies.find(m => m._id === match.params.movieId)} user={user}
+               setUser={user => this.setUser(user)} onBackClick={() => history.goBack()}
+                  />
+              </Col>
+          }} 
